e2fd191ca0f16a3ced63f7bd1f8fb9aa4a0bc905
jtblackk <jtblack@g.clemson.edu>
2021-01-28 20:45:36 -0500

    added url routing to the website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Index from './pages/Index'
+import Signup from './pages/Signup'
+import PageNotFound from './pages/PageNotFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>put some layout here</p>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/signup" component={Signup} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
