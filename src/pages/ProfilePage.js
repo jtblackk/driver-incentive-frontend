997bd9b3a0745a67f3a18f04b8390167b493f5fc
jtblackk <jtblack@g.clemson.edu>
2021-02-07 10:57:30 -0500

    Filled out the profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,15 +1,13 @@
+import React, { useEffect, useState } from 'react'
+
 import TopAppBar from '../Components/TopAppBar'
-import { Grid } from '@material-ui/core'
+import UserProfileCard from '../Components/UserProfileCard'
 
 function ProfilePage() {
   return (
     <div>
       <TopAppBar pageTitle="Your profile" />
-      <Grid container justify="space-evenly">
-        <Grid item>
-          <p>filler</p>
-        </Grid>
-      </Grid>
+      <UserProfileCard profileEmail={'Driver@gmail.com'} />
     </div>
   )
 }
