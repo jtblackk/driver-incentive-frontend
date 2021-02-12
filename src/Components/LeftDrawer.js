91ce37f1cce803f4c9590b5cba3efdaf5edf87c1
jtblackk <jtblack@g.clemson.edu>
2021-02-11 22:13:41 -0500

    created a placeholder left drawer

---
+++ src/Components/LeftDrawer.js
@@ -0,0 +1,112 @@
+import {
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
+
+import { useHistory } from 'react-router-dom'
+
+import React from 'react'
+
+import { DRAWER_WIDTH } from '../Helpers/Constants'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: DRAWER_WIDTH,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${DRAWER_WIDTH}px)`,
+      marginLeft: DRAWER_WIDTH,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: DRAWER_WIDTH,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}))
+
+const LeftDrawer = () => {
+  const classes = useStyles()
+  let history = useHistory()
+
+  let pages = [
+    {
+      name: 'Home',
+      route: '/',
+      icon: <InboxIcon />,
+    },
+    {
+      name: 'Profile',
+      route: '/profile',
+      icon: <InboxIcon />,
+    },
+  ]
+
+  return (
+    <div>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {pages.map((page, index) => (
+            <ListItem
+              button
+              key={page.name}
+              onClick={() => {
+                history.push(page.route)
+              }}
+            >
+              <ListItemIcon>{page.icon}</ListItemIcon>
+              <ListItemText primary={page.name} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  )
+}
+
+export default LeftDrawer
