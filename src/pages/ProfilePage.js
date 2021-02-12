1b72f548d3396b349360a61a01474622c689af1e
jtblackk <jtblack@g.clemson.edu>
2021-02-11 22:31:33 -0500

    added layout styling to profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,13 +1,56 @@
 import React, { useEffect, useState } from 'react'
 
+import { makeStyles } from '@material-ui/core/styles'
+
 import TopAppBar from '../Components/TopAppBar'
 import UserProfileCard from '../Components/UserProfileCard'
+import { DRAWER_WIDTH } from '../Helpers/Constants'
+import LeftDrawer from '../Components/LeftDrawer'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: DRAWER_WIDTH,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${DRAWER_WIDTH}px)`,
+      marginLeft: DRAWER_WIDTH,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: DRAWER_WIDTH,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}))
 
 function ProfilePage() {
+  const classes = useStyles()
+
   return (
-    <div>
+    <div className={classes.root}>
+      <LeftDrawer />
       <TopAppBar pageTitle="Your profile" />
-      <UserProfileCard profileEmail={'Driver@gmail.com'} />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <UserProfileCard profileEmail={'Driver@gmail.com'} />
+      </main>
     </div>
   )
 }
