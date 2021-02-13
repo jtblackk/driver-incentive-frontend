e6d825cfbb1b7a2b0bd89cd7b690805caf0fc091
jtblackk <jtblack@g.clemson.edu>
2021-02-12 20:26:40 -0500

    cleaned up left drawer

--- src/Components/LeftDrawer.js
+++ src/Components/LeftDrawer.js
@@ -1,8 +1,8 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import {
   Divider,
   Drawer,
-  Grid,
   List,
   ListItem,
   ListItemText,
@@ -11,132 +11,111 @@ import ListItemIcon from '@material-ui/core/ListItemIcon'
 import HomeIcon from '@material-ui/icons/Home'
 import PersonIcon from '@material-ui/icons/Person'
 import { makeStyles } from '@material-ui/core/styles'
-
 import MeetingRoomIcon from '@material-ui/icons/MeetingRoom'
 import SettingsIcon from '@material-ui/icons/Settings'
-
-import { useHistory } from 'react-router-dom'
 import { Auth } from 'aws-amplify'
-
 import { DRAWER_WIDTH } from '../Helpers/Constants'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
+const pages = [
+  {
+    name: 'Home',
+    route: '/',
+    icon: <HomeIcon />,
   },
+  {
+    name: 'My profile',
+    route: '/profile',
+    icon: <PersonIcon />,
+  },
+]
+
+const useStyles = makeStyles((theme) => ({
   drawer: {
     [theme.breakpoints.up('sm')]: {
       width: DRAWER_WIDTH,
       flexShrink: 0,
     },
   },
-  appBar: {
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${DRAWER_WIDTH}px)`,
-      marginLeft: DRAWER_WIDTH,
-    },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
-  },
-  // necessary for content to be below app bar
   toolbar: theme.mixins.toolbar,
   drawerPaper: {
     width: DRAWER_WIDTH,
   },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
 }))
 
 const LeftDrawer = () => {
-  const classes = useStyles()
   let history = useHistory()
-
-  let top_pages = [
-    {
-      name: 'Home',
-      route: '/',
-      icon: <HomeIcon />,
-    },
-    {
-      name: 'My profile',
-      route: '/profile',
-      icon: <PersonIcon />,
-    },
-  ]
+  const classes = useStyles()
 
   return (
-    <div>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      anchor="left"
+    >
+      <div className={classes.toolbar} />
 
+      <Divider />
+
+      {/* top pages */}
+      <List>
+        {pages.map((page, index) => (
+          <ListItem
+            button
+            key={page.name}
+            onClick={() => {
+              history.push(page.route)
+            }}
+          >
+            <ListItemIcon>{page.icon}</ListItemIcon>
+            <ListItemText primary={page.name} />
+          </ListItem>
+        ))}
         <Divider />
+      </List>
+
+      {/* bottom pages */}
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+          overflow: 'hidden',
+        }}
+      >
+        <Divider />
+
+        {/* settings */}
         <List>
-          {top_pages.map((page, index) => (
-            <ListItem
-              button
-              key={page.name}
+          <ListItem button key={'Settings'}>
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary={'Settings'}
               onClick={() => {
-                history.push(page.route)
+                history.push('/settings')
               }}
-            >
-              <ListItemIcon>{page.icon}</ListItemIcon>
-              <ListItemText primary={page.name} />
-            </ListItem>
-          ))}
-          <Divider />
+            />
+          </ListItem>
+
+          {/* sign out */}
+          <ListItem button key={'Sign out'}>
+            <ListItemIcon>
+              <MeetingRoomIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary={'Sign out'}
+              onClick={() => {
+                Auth.signOut()
+              }}
+            />
+          </ListItem>
         </List>
-
-        <div
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            width: '100%',
-            overflow: 'hidden',
-          }}
-        >
-          <Divider />
-
-          <List>
-            <ListItem button key={'Settings'}>
-              <ListItemIcon>
-                <SettingsIcon />
-              </ListItemIcon>
-              <ListItemText
-                primary={'Settings'}
-                onClick={() => {
-                  history.push('/settings')
-                }}
-              />
-            </ListItem>
-
-            <ListItem button key={'Sign out'}>
-              <ListItemIcon>
-                <MeetingRoomIcon />
-              </ListItemIcon>
-              <ListItemText
-                primary={'Sign out'}
-                onClick={() => {
-                  Auth.signOut()
-                }}
-              />
-            </ListItem>
-          </List>
-        </div>
-      </Drawer>
-    </div>
+      </div>
+    </Drawer>
   )
 }
 
