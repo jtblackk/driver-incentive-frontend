c3b71f4acbc0e34c76844162dd53c493c95592a7
jtblackk <jtblack@g.clemson.edu>
2021-02-13 12:16:26 -0500

    loading screeen while fetching data

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -8,6 +8,7 @@ import { makeStyles } from '@material-ui/core/styles'
 
 import { Auth } from 'aws-amplify'
 import { DRAWER_WIDTH } from '../Helpers/Constants'
+import { CircularProgress, Grid } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,6 +23,17 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(3),
   },
 }))
+
+// loading icon that shows up when waiting for data to load
+function LoadingIcon() {
+  return (
+    <Grid container justify="center">
+      <Grid item xs={8} sm={6} md={5} lg={3} align="center">
+        <CircularProgress></CircularProgress>
+      </Grid>
+    </Grid>
+  )
+}
 
 function IndexPage() {
   const classes = useStyles()
@@ -44,6 +56,11 @@ function IndexPage() {
       fetch(GET_USERDATA_URL)
         .then((response) => response.json())
         .then((data) => {
+          if (
+            !userProfileDetails.AccountType ||
+            userProfileDetails.AccountType === ''
+          )
+            history.push('/account-setup')
           setUserProfileDetails({
             Email_ID: data.Item.Email_id,
             FirstName: data.Item.FirstName,
@@ -53,8 +70,6 @@ function IndexPage() {
             SponsorID: data.Item.SponsorEmailID,
             TotalPoints: data.Item.TotalPoints,
           })
-          if ((userProfileDetails.AccountType = ''))
-            history.push('/account-setup')
         })
     })
   }, [])
@@ -65,48 +80,55 @@ function IndexPage() {
   ) {
     return <WaitingForApplicationApprovalScreen />
   } else {
-    return (
-      <div className={classes.root}>
-        {/* layout stuff */}
-        <TopAppBar pageTitle="Welcome"></TopAppBar>
-        <LeftDrawer />
+    if (
+      !userProfileDetails.AccountType ||
+      userProfileDetails.AccountType === ''
+    ) {
+      return <LoadingIcon />
+    } else {
+      return (
+        <div className={classes.root}>
+          {/* layout stuff */}
+          <TopAppBar pageTitle="Welcome"></TopAppBar>
+          <LeftDrawer />
 
-        {/* page content (starts after first div) */}
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-            dolor purus non enim praesent elementum facilisis leo vel. Risus at
-            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
-            rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
-            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-            vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-            ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-            elementum integer enim neque volutpat ac tincidunt. Ornare
-            suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-            volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
-            Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
-            ornare massa eget egestas purus viverra accumsan in. In hendrerit
-            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
-            aliquam sem et tortor. Habitant morbi tristique senectus et.
-            Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
-            euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-            ultrices sagittis orci a.
-          </Typography>
-        </main>
-      </div>
-    )
+          {/* page content (starts after first div) */}
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Typography paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Rhoncus dolor purus non enim praesent elementum facilisis leo vel.
+              Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+              gravida rutrum quisque non tellus. Convallis convallis tellus id
+              interdum velit laoreet id donec ultrices. Odio morbi quis commodo
+              odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum
+              est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+              Metus vulputate eu scelerisque felis imperdiet proin fermentum
+              leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
+              lobortis feugiat vivamus at augue. At augue eget arcu dictum
+              varius duis at consectetur lorem. Velit sed ullamcorper morbi
+              tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+            </Typography>
+            <Typography paragraph>
+              Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+              ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+              elementum integer enim neque volutpat ac tincidunt. Ornare
+              suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+              volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
+              Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
+              ornare massa eget egestas purus viverra accumsan in. In hendrerit
+              gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
+              aliquam sem et tortor. Habitant morbi tristique senectus et.
+              Adipiscing elit duis tristique sollicitudin nibh sit. Ornare
+              aenean euismod elementum nisi quis eleifend. Commodo viverra
+              maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin
+              aliquam ultrices sagittis orci a.
+            </Typography>
+          </main>
+        </div>
+      )
+    }
   }
 }
 
