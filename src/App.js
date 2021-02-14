3448ebda3c0a4ffcdef981539fd0603017868884
jtblackk <jtblack@g.clemson.edu>
2021-02-13 20:13:44 -0500

    rremoved redundant page routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './App.css'
 import Amplify from 'aws-amplify'
 import awsconfig from './aws-exports'
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { withAuthenticator } from 'aws-amplify-react'
-
-import IndexPage from './pages/IndexPage'
-import SignupPage from './pages/SignupPage'
-import AdminHomePage from './pages/AdminHomePage'
-import SponsorHomePage from './pages/SponsorHomePage'
-import DriverHomePage from './pages/DriverHomePage'
 import PageNotFoundPage from './pages/PageNotFoundPage'
 import AccountSetupPage from './pages/AccountSetupPage'
 import ProfilePage from './pages/ProfilePage'
-
-import '@aws-amplify/ui/dist/style.css'
+import IndexPage from './pages/IndexPage'
 import ApplicationPage from './pages/ApplicationPage'
-import Bootstrap from 'aws-amplify-react/lib-esm/AmplifyTheme'
+import '@aws-amplify/ui/dist/style.css'
+
 Amplify.configure(awsconfig)
 
 function App() {
@@ -26,10 +19,6 @@ function App() {
         <Switch>
           <Route exact path="/" component={IndexPage} />
           <Route exact path="/profile" component={ProfilePage} />
-          <Route exact path="/signup" component={SignupPage} />
-          <Route exact path="/driver" component={DriverHomePage} />
-          <Route exact path="/sponsor" component={SponsorHomePage} />
-          <Route exact path="/admin" component={AdminHomePage} />
           <Route exact path="/account-setup" component={AccountSetupPage} />
           <Route exact apth="/apply" component={ApplicationPage} />
           <Route component={PageNotFoundPage} />
@@ -41,7 +30,6 @@ function App() {
 
 // export default App
 export default withAuthenticator(App, {
-  // theme: Bootstrap,
   usernameAttributes: 'email',
   signUpConfig: {
     hiddenDefaults: ['phone_number'],
