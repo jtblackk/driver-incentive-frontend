a899f0180c01b9614829cb5d6ab6383df73d1b70
jtblackk <jtblack@g.clemson.edu>
2021-02-26 20:55:33 -0500

    fixed issue with JS strings and SQL queries

--- src/Components/DriverApplicationCard.js
+++ src/Components/DriverApplicationCard.js
@@ -10,6 +10,8 @@ import {
 } from '@material-ui/core'
 import LoadingIcon from './LoadingIcon'
 
+import { escape } from 'sqlstring'
+
 const DriverApplicationCard = (props) => {
   let history = useHistory()
   const [applicationDetails, setApplicationDetails] = useState({
@@ -135,14 +137,16 @@ const DriverApplicationCard = (props) => {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
                   applicant_email: applicationDetails.Email_ID,
-                  FirstName: applicationDetails.FirstName,
-                  LastName: applicationDetails.LastName,
-                  UserBio: applicationDetails.UserBio,
                   sponsor_email: applicationDetails.Sponsor,
-                  comments: applicationDetails.Comments,
+                  FirstName: applicationDetails.FirstName.replaceAll("'", "''"),
+                  LastName: applicationDetails.LastName.replaceAll("'", "''"),
+                  comments: applicationDetails.Comments.replaceAll("'", "''"),
+                  UserBio: applicationDetails.UserBio.replaceAll("'", "''"),
                 }),
               }
               fetch(SEND_APPLICATION_URL, requestOptions)
+
+              // applicationDetails.UserBio.replace("'", "''")
 
               // console.log({
               //   applicant_email: applicationDetails.Email_ID,
