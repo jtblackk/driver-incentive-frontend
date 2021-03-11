ca2e9dd7185d4fd73ecf85668db3f0ee61e1fb99
jtblackk <jtblack@g.clemson.edu>
2021-03-10 22:58:10 -0500

    added ability to get user details by username

--- src/Helpers/CommonFunctions.js
+++ src/Helpers/CommonFunctions.js
@@ -1,12 +1,16 @@
 import Amplify, { Auth } from 'aws-amplify'
 import awsconfig from '../aws-exports'
 
-export default async function getUserDetails() {
-  const user = await Auth.currentAuthenticatedUser()
-  let user_email = user.attributes.email
+export default async function getUserDetails(username) {
+  let user_email = null
+  if (username) {
+    user_email = username
+  } else {
+    const user = await Auth.currentAuthenticatedUser()
+    user_email = user.attributes.email
+  }
 
   // get user's data
-  // TODO: make this get data from the new user table (need to create a get request for that table)
   let GET_USERDATA_URL = `https://esqgp2f0t8.execute-api.us-east-1.amazonaws.com/dev/getuserdetails?Username=${user_email}`
   const response = await fetch(GET_USERDATA_URL)
   const data = await response.json()
@@ -18,9 +22,6 @@ export default async function getUserDetails() {
     Bio: parsed_details.Items[0].Bio.S,
     AccountType: parsed_details.Items[0].AccountType.S,
     AccountStatus: parseInt(parsed_details.Items[0].AccountStatus.S),
-    // TODO: make sure that any component that needs these three pulls them on their own
-    // SponsorEmailID: data.Item.SponsorEmailID,
-    // TotalPoints: data.Item.TotalPoints,
   }
 
   return profile_details
