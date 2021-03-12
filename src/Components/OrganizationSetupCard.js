a216f210e2cd1e22044a3d56bd708b3e216363d5
jtblackk <jtblack@g.clemson.edu>
2021-03-11 21:29:49 -0500

    validation and submission for org setup

--- src/Components/OrganizationSetupCard.js
+++ src/Components/OrganizationSetupCard.js
@@ -1,7 +1,59 @@
 import { Button, Grid, Paper, TextField } from '@material-ui/core'
-import React from 'react'
+import { toLower } from 'lodash'
+import React, { useContext, useEffect, useState } from 'react'
+import { UserContext } from '../Helpers/UserContext'
+import LoadingIcon from './LoadingIcon'
+import { useHistory } from 'react-router-dom'
 
 const OrganizationSetupCard = () => {
+  const [organizationName, setOrganizationName] = useState(null)
+  const [isLoading, setIsLoading] = useState(null)
+  const [organizationList, setOrganizationList] = useState(null)
+  const [helperText, setHelperText] = useState(null)
+
+  let history = useHistory()
+  const userData = useContext(UserContext).user
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    let GET_SPONSORDATA_URL =
+      'https://2cw17jd576.execute-api.us-east-1.amazonaws.com/dev/sponsorlist'
+    fetch(GET_SPONSORDATA_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        let clean_sonsor_list = []
+        let sponsor_array = JSON.parse(data.body.toString()).Items
+        sponsor_array.forEach((val) => {
+          clean_sonsor_list.push(toLower(val.Organization.S))
+        })
+
+        console.log(clean_sonsor_list)
+        setOrganizationList(clean_sonsor_list)
+      })
+      .then(() => {
+        setIsLoading(false)
+      })
+  }, [])
+
+  if (!isLoading) {
+  } else {
+    return (
+      <Grid container justify="center">
+        <Grid item container xs={12} direction="column" spacing={2}>
+          <Grid item xs={12}>
+            <br />
+          </Grid>
+          <Grid item xs={12}>
+            <LoadingIcon />
+          </Grid>
+          <Grid item xs={12}>
+            <br />
+          </Grid>
+        </Grid>
+      </Grid>
+    )
+  }
   return (
     <Grid container justify="center">
       <Grid item container xs={12} direction="column" spacing={2}>
@@ -10,10 +62,13 @@ const OrganizationSetupCard = () => {
           <TextField
             fullWidth
             id="OrganizationName"
+            error={helperText}
+            helperText={helperText}
             variant="filled"
             label="Organization name"
             onChange={(event) => {
-              // TODO: save input in state
+              setHelperText(null)
+              setOrganizationName(event.target.value)
             }}
           ></TextField>
         </Grid>
@@ -22,9 +77,32 @@ const OrganizationSetupCard = () => {
             fullWidth
             variant="contained"
             color="primary"
-            onCLick={(event) => {
-              //   TODO: validate input
-              // TODO: set organization name in database
+            onClick={(event) => {
+              // validate input
+              if (!organizationName) {
+                setHelperText('Please provide an organization name')
+              } else if (organizationList.includes(toLower(organizationName))) {
+                setHelperText('Must choose a unique organization name')
+              } else {
+                // save organization name
+                console.log(organizationName)
+                setIsLoading(true)
+                let SAVE_USER_PROFILE_URL =
+                  'https://u902s79wa3.execute-api.us-east-1.amazonaws.com/dev/saveuserdetails'
+                let requestOptions = {
+                  method: 'POST',
+                  headers: { 'Content-Type': 'application/json' },
+                  body: JSON.stringify({
+                    Username: userData.Username,
+                    Organization: organizationName,
+                  }),
+                }
+                fetch(SAVE_USER_PROFILE_URL, requestOptions).then(() => {
+                  // reroute the user to index page
+                  setIsLoading(false)
+                  history.push('/')
+                })
+              }
             }}
           >
             Create organization
