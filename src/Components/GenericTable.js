ef9d88d6eda3263b1dc0729503650c15c102ba46
jtblackk <jtblack@g.clemson.edu>
2021-03-12 17:44:54 -0500

    added loading icon to generic table

--- src/Components/GenericTable.js
+++ src/Components/GenericTable.js
@@ -10,6 +10,7 @@ import Paper from '@material-ui/core/Paper'
 import { Box, TableSortLabel } from '@material-ui/core'
 
 import orderBy from 'lodash/orderBy'
+import LoadingIcon from './LoadingIcon'
 
 require('datejs')
 
@@ -116,7 +117,6 @@ export default function GenericTable(props) {
       </TableContainer>
     )
   } else {
-    // TODO: handle this loading case
-    return <div></div>
+    return <LoadingIcon />
   }
 }
