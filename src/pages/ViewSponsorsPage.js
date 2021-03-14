780071fc5114c5eaa8a72e175e914a325e823a7d
jtblackk <jtblack@g.clemson.edu>
2021-03-13 23:01:47 -0500

    used live data on 'view my sponsors' page

--- src/pages/ViewSponsorsPage.js
+++ src/pages/ViewSponsorsPage.js
@@ -56,21 +56,97 @@ const ViewSponsorsPage = () => {
 
   useEffect(() => {
     setIsLoading(true)
+    ;(async () => {
+      // get all the potential sponsors
+      let GET_SPONSORLIST_URL =
+        'https://2cw17jd576.execute-api.us-east-1.amazonaws.com/dev/sponsorlist'
+      let sponsorlist_response = await fetch(GET_SPONSORLIST_URL)
+      let sponsorlist_data = await sponsorlist_response.json()
+      let sponsorlist_array = JSON.parse(sponsorlist_data.body.toString()).Items
+      let sponsorlist_formatted = sponsorlist_array.map((element) => {
+        return {
+          Username: element.Username.S,
+          FirstName: element.FirstName.S,
+          LastName: element.LastName.S,
+          Organization: element.Organization.S,
+        }
+      })
 
+      let GET_DRIVERS_SPONSORS_URL = `https://8mhdaeq2kl.execute-api.us-east-1.amazonaws.com/dev/getuserdetails/?DriverID=${userData.Username}`
+      let partnered_sponsors_response = await fetch(GET_DRIVERS_SPONSORS_URL)
+      let partnered_sponsors_data = await partnered_sponsors_response.json()
+      let partnered_sponsors_array = JSON.parse(
+        partnered_sponsors_data.body.toString(),
+      ).Items
+      let partnered_sponsors_formatted = partnered_sponsors_array.map(
+        (element) => {
+          return {
+            SponsorshipID: element.SponsorshipID.S,
+            SponsorID: element.SponsorID.S,
+            DriverID: element.DriverID.S,
+            AppDecisionDate: element.AppDecisionDate.S,
+            Points: parseInt(element.Points.N),
+            PointDollarRatio: parseFloat(element.PointDollarRatio.N),
+            Status: parseInt(element.Status.N),
+          }
+        },
+      )
+      let partnered_sponsors_names = partnered_sponsors_array.map((element) => {
+        return element.SponsorID.S
+      })
+
+      let active_sponsors_data = sponsorlist_formatted.map((element) => {
+        if (partnered_sponsors_names.includes(element.Username)) {
+          return {
+            SponsorID: element.Username,
+            FirstName: element.FirstName,
+            LastName: element.LastName,
+            Organization: element.Organization,
+            ...partnered_sponsors_formatted.find((element_2) => {
+              return element_2.DriverID === userData.Username
+            }),
+          }
+        } else {
+          return null
+        }
+      })
+
+      let active_sponsors_table_data = active_sponsors_data
+        .filter((element) => element)
+        .map((element) => {
+          return {
+            SponsorshipID: element.SponsorshipID,
+            Sponsor:
+              element.Organization +
+              ': ' +
+              element.FirstName +
+              ' ' +
+              element.LastName,
+            TotalPoints: parseInt(element.Points),
+            StartDat: element.AppDecisionDate,
+          }
+        })
+
+      console.log(active_sponsors_table_data)
+      console.log(partnered_sponsors_formatted)
+      setTable1Data(active_sponsors_table_data)
+    })().then(() => {
+      setIsLoading(false)
+    })
     // TODO: pull sponsorship data, clean it up into needed format. waiting on api to pull sponsorship data by driver
 
-    setTable1Data([
-      {
-        SponsorshipID: 'jtblack@g.clemson.edu#themeantruckinteam@gmail.com',
-        Sponsor: 'Progressive: Flo',
-        TotalPoints: 150,
-      },
-      {
-        SponsorshipID: 'jtblack@g.clemson.edu#jtblack@g.clemson.edu',
-        Sponsor: 'Clemson: Jeff Black',
-        TotalPoints: 0,
-      },
-    ])
+    // setTable1Data([
+    //   {
+    //     SponsorshipID: 'jtblack@g.clemson.edu#themeantruckinteam@gmail.com',
+    //     Sponsor: 'Progressive: Flo',
+    //     TotalPoints: 150,
+    //   },
+    //   {
+    //     SponsorshipID: 'jtblack@g.clemson.edu#jtblack@g.clemson.edu',
+    //     Sponsor: 'Clemson: Jeff Black',
+    //     TotalPoints: 0,
+    //   },
+    // ])
     setTable1HeadCells([
       {
         id: 'Sponsor',
@@ -84,8 +160,13 @@ const ViewSponsorsPage = () => {
         isDate: false,
         width: 100,
       },
+      {
+        id: 'StartDate',
+        label: 'Sponsored since',
+        isDate: false,
+        width: 100,
+      },
     ])
-    setIsLoading(false)
   }, [pageUpdate])
 
   if (!isLoading) {
