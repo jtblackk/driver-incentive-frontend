bbc51b27f00772d746b07ae0f909b207cb07a197
jtblackk <jtblack@g.clemson.edu>
2021-03-20 18:57:00 -0400

    added ebay api support

--- src/Components/AddCatalogItemDialog.js
+++ src/Components/AddCatalogItemDialog.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
@@ -6,6 +6,10 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import { Divider, Grid, Typography } from '@material-ui/core'
+import LoadingIcon from './LoadingIcon'
+import Ebay from 'ebay-node-api'
+
+let eBay = require('ebay-node-api')
 
 export default function AddCatalogItemDialog(props) {
   const handleClickOpen = () => {
@@ -15,6 +19,31 @@ export default function AddCatalogItemDialog(props) {
   const handleClose = () => {
     props.dialogProps.setAddItemDialogIsOpenState(false)
   }
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [ebayItems, setEbayItems] = useState(null)
+  useEffect(() => {
+    setIsLoading(true)
+    ;(async () => {
+      const FETCH_EBAY_ITEMS_URL =
+        'https://0hcub33ona.execute-api.us-east-1.amazonaws.com/dev/ebay'
+      let ebay_items_raw = await fetch(FETCH_EBAY_ITEMS_URL)
+      let ebay_items_json = await ebay_items_raw.json()
+      let ebay_items_array = ebay_items_json.Product
+      console.log(ebay_items_array)
+      setEbayItems(ebayItems)
+    })().then(() => {
+      setIsLoading(false)
+    })
+
+    // setEbay(Ebay({ clientID: 'wtf' }))
+    // new eBay({
+    //   clientID: 'effBlac-DriverIn-PRD-4418204ca-4dac81da',
+    // }),
+
+    setIsLoading(false)
+  }, [])
 
   return (
     <div>
@@ -28,14 +57,18 @@ export default function AddCatalogItemDialog(props) {
           {'Select items to add'}
         </DialogTitle>
         <DialogContent>
-          <Grid container spacing="2">
-            <Grid item xs={12}>
-              <Typography>
-                display selectable items here (filter out items that are already
-                owned)
-              </Typography>
+          {isLoading ? (
+            <LoadingIcon />
+          ) : (
+            <Grid container spacing="2">
+              <Grid item xs={12}>
+                <Typography>
+                  display selectable items here (filter out items that are
+                  already owned)
+                </Typography>
+              </Grid>
             </Grid>
-          </Grid>
+          )}
         </DialogContent>
       </Dialog>
     </div>
